572ac97467cbc92e5068b5e74c28974c98d8da6a
John Pham <mr.john.pham@gmail.com>
2020-03-23 07:29:10 -0400

    Mark off closing brace and parenthesis

--- heatmap.js
+++ heatmap.js
@@ -21,8 +21,11 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
     .then(data => dataset = data.slice())
     .then(() => {
         
-    }
-);
+    /**  */
+
+
+
+}); // Closing brace for last then statement and closing parenthesis for fetch statement
 
 /*** Legend code */
 
@@ -40,6 +43,7 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 [ ] Create heat map
     [ ] The x-axis should have tick labels with the years between 1754 and 2015
     [ ] The y-axis should have tick labels with the full name of the months of the year
+    [ ] Figure out the length and width of each cell
 [ ] Create legend
     [ ] The legend should have at least 4 different fill colors
 */
