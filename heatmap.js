e673c4ff27a826377a7f09aed719aa883053560b
John Pham <mr.john.pham@gmail.com>
2020-03-23 07:08:44 -0400

    Initialize global variables: dataset

--- heatmap.js
+++ heatmap.js
@@ -1,2 +1,13 @@
 'use strict';
 
+/*** Initialize global variables */
+let dataset = [];
+
+/*** Heatmap code */
+/** Fetch data */
+fetch('tps://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
+    .then(response => response.json())
+    .then(data => dataset = data.slice())
+    .then(() => {
+
+    }
