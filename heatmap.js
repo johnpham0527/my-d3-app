c52cec6a8896fbd54088ec8ba988568246b57c9f
John Pham <mr.john.pham@gmail.com>
2020-03-23 07:09:42 -0400

    Create tooltip const variable

--- heatmap.js
+++ heatmap.js
@@ -3,6 +3,14 @@
 /*** Initialize global variables */
 let dataset = [];
 
+// Tooltip
+const tooltip = d3.select("body")
+                   .append("div")
+                   .attr("class","tooltip")
+                   .attr("id","tooltip")
+                   .style("opacity",0)
+
+                
 /*** Heatmap code */
 /** Fetch data */
 fetch('tps://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json')
