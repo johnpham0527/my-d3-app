4e59304584282678d0610642411428fee16d6b5a
John Pham <mr.john.pham@gmail.com>
2020-03-26 21:27:21 -0400

    Squash bug: tooltip appears

--- heatmap.js
+++ heatmap.js
@@ -13,9 +13,9 @@ const tooltip = d3.select("body")
 
 /** Global heat map variables */ 
 /* Graph dimensions */
-const w = 1310;
-const h = 420;
-const padding = 72;
+const w = 1965;
+const h = 610;
+const padding = 80;
 
 /* Time units */
 let parseDate = d3.timeParse('%Y');
@@ -140,20 +140,23 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
             .attr("data-temp", (d) => baseTemp + d.variance)
 
         /** Tool tip */
-        /* Mouseover */
             .on("mouseover", (d) => {
-                tooltip.transition().duration(200)
-                    .attr("id", "tooltip")
-                    .attr("data-year", d.year)
-                    .html(d.year + " - " + d.month)
-                    .style("left", d3.event.pageX + 5 + "px") //x-axis offset
-                    .style("top", d3.event.pageY - 5 + "px") //y-axis offset
+                tooltip.style("opacity", 0.8)
+                .attr("id", "tooltip")
+                .attr("data-year", d.year)
+                .attr("data-variance", d.variance)
+                .attr("data-temp", baseTemp + d.variance)
+                .html( () => {
+                    let outputTemp = Math.round((baseTemp + d.variance)*100)/100; //rounds up to 2 decimal places
+                    let outputVariance = Math.round(d.variance*100)/100; //rounds up to 2 decimal places
+                    let outputSign = outputVariance > 0 ? "+" : ""; //ternary operator: assign plus sign if outputVariance is positive
+                    return d.year + " - " + month(d.month) + "<br>" + outputTemp + "&#8451<br>" + outputSign + outputVariance + "&#8451";
+                })     
+                .style("left", d3.event.pageX + 5 + "px")
+                .style("top", d3.event.pageY - 5 + "px")
             })
-
-        /* Mouse out */
             .on("mouseout", (d) => {
-                tooltip.transition().duration(200)
-                .style("opacity", 0)
+                tooltip.style("opacity", 0)
             });
 
         /** Set up x-axis*/
@@ -187,36 +190,3 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
             .style("fill", (d) => d)
 
 }); // Closing brace for last then statement and closing parenthesis for fetch statement
-
-
-/*** To-do's */
-/*
-[X] Study the data: 
-    [X] Link: https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json
-    [X] What is this heat map about? Climate change trends by month and by year
-    [X] Determine the min and max values
-    [X] Determine how to set up the x-axis: years
-    [X] Determine how to set up the y-axis: month
-[ ] Create heat map separately from legend
-    [X] Create separate sections for legend and heatmap
-[ ] Create heat map
-    [X] The x-axis should have tick labels with the years between 1754 and 2015
-    [X] The y-axis should have tick labels with the full name of the months of the year
-    [X] Figure out the length and width of each cell
-    [X] Determine at least four gradient colors
-        Midnight blue -> medium blue -> royal blue - > light cyan -> light yellow -> peach puff -> 
-        salmon <- crimson <- Dark red
-    [ ] Debug: the height for December cells isn't quite right
-    [ ] Debug: the cells are shooting past the x-axis
-    [X] Debug: there is white space between each cell width
-    [X] A few of the colors are not on the color scale; decided to set default color to crimsonred
-    [ ] For svg.selectAll, implement style fill use (d,i) => ... instead of using a for loop, which is slow
-[ ] Create the tool tips
-    [ ] Debug why tooltip doesn't appear
-[ ] Create legend
-    [ ] Create a legend axis
-    [ ] Create grid boxes
-    [ ] Leverage unitTicks array to generate tick labels
-    [ ] Leverage unitTicks array and fill to generate gradient color boxes
-[ ] Delete commented out or unneeded code
-*/
