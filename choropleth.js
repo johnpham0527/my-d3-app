457d09bc050a2d825bd4c2e1f9e0a995302cca23
John Pham <mr.john.pham@gmail.com>
2020-03-27 10:20:38 -0400

    Use legendCellHeight

--- choropleth.js
+++ choropleth.js
@@ -14,7 +14,7 @@ const tooltip = d3.select("body")
 
 /** Global choropleth map variables */
 /* Graph dimensions */
-const w = 1310;
+const w = 1280;
 const h = 420;
 const padding = 80;
 
@@ -33,7 +33,7 @@ const colorArray = ["lightgreen", "palegreen", "darkseagreen", "mediumseagreen",
 const svg = d3.select("#choropleth")
     .append("svg")
     .attr("width",w)
-    .attr("height",h)
+    .attr("height",h);
 
 /*** Choropleth code */
 
@@ -43,44 +43,44 @@ fetch('https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/fo
     .then(data => dataset = data)
     .then(() => {
 
-    /** Set up local choropleth map variables */
+        /** Set up local choropleth map variables */
 
-    const legendCellHeight = 4;
-    const legendCellWidth = 12;
+        const legendCellHeight = 4;
+        const legendCellWidth = 12;
 
-    const legendTickArray = ["3%", "12%", "21%", "30%", "39%", "48%", "57%", "66%"];
+        const legendTickArray = ["3%", "12%", "21%", "30%", "39%", "48%", "57%", "66%"];
 
-    const legendAxis = d3.axisBottom(legendScale)
-            .tickValues(legendTickArray)
-            .tickFormat(d3.format(".2f"));
+        const legendAxis = d3.axisBottom(legendScale)
+                .tickValues(legendTickArray)
+                .tickFormat(d3.format(".2f"));
 
-    /** Map topology */
+        /** Map topology */
 
-    /** Map dataset to graph */
+        /** Map dataset to graph */
 
-    /** Insert data attributes */
+        /** Insert data attributes */
 
-    /** Display tooltip */
+        /** Display tooltip */
 
-    /** Display legend axis*/
-    svg.append("g")
-        .attr("transform", "translate(" + (w-padding-legendCellWidth*colorArray.length) + "," + (padding/12 + cellHeight - 1) + ")")
-        .call(legendAxis);
+        /** Display legend axis*/
+        svg.append("g")
+            .attr("transform", "translate(" + (w-padding-legendCellWidth*colorArray.length) + "," + (padding/12 + cellHeight - 1) + ")")
+            .call(legendAxis);
 
-    /** Display legend cells */
-    svg.append("g")
-        .attr("id","legend")
-        .selectAll("rect")
-        .data(colorArray) //use the color array as the dataset
-        .enter() 
-        .append("rect")
-        .attr("x", (d, i) => (w-padding-legendCellWidth*colorArray.length) + legendCellWidth*i) //place it in the top right location
-        .attr("y", padding/12)
-        .attr("width", legendCellWidth)
-        .attr("height", cellHeight)
-        .style("fill", (d) => d)
+        /** Display legend cells */
+        svg.append("g")
+            .attr("id","legend")
+            .selectAll("rect")
+            .data(colorArray) //use the color array as the dataset
+            .enter() 
+            .append("rect")
+            .attr("x", (d, i) => (w-padding-legendCellWidth*colorArray.length) + legendCellWidth*i) //place it in the top right location
+            .attr("y", padding/12)
+            .attr("width", legendCellWidth)
+            .attr("height", legendCellHeight)
+            .style("fill", (d) => d);
 
-    }); //Closing brace for last 'then' statement and closing parenthesis for fetch statement
+}); //Closing brace for last 'then' statement and closing parenthesis for fetch statement
 
 /*** To-do's */
 /*
