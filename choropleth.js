37d5d3a2133de8f097fd929da4887ede10bc37ab
John Pham <mr.john.pham@gmail.com>
2020-03-27 09:49:40 -0400

    Create comments for various sections of code

--- choropleth.js
+++ choropleth.js
@@ -1,2 +1,38 @@
 "use strict";
 
+/*** Initialize global variables */
+/** Dataset */
+
+/** Tooltip */
+
+/** Global choropleth map variables */
+/* Graph dimensions */
+
+/* Topology units */
+
+/* Get Functions */
+
+/* SVG const */
+
+/*** Choropleth code */
+
+/** Fetch data */
+
+    /** Local choropleth map variables */
+
+    /** Map topology */
+
+    /** Map dataset to graph */
+
+    /** Data attributes */
+
+    /** Tooltip */
+
+    /** Legend axis*/
+
+    /** Legend cells */
+
+
+    //Closing brace
+
+/*** To-do's */
