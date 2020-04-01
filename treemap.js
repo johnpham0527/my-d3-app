a282837e6c812a86bf67712e8467eb1cea5b70ca
John Pham <mr.john.pham@gmail.com>
2020-04-01 14:18:31 -0400

    Attempt to implement treemap

--- treemap.js
+++ treemap.js
@@ -37,10 +37,59 @@ fetch(VIDEO_GAME_SALES_URL)
     .then( response => response.json())
     .then( videoGameData => { 
 
+    let root = d3.hierarchy(videoGameData) //create data structure that represents a hierarchy
+        .sum( d => d.value) //traverses the tree and sets .value on each node to the sum of its children
+        .sort( (a,b) => b.value - a.value)
 
+    let treemapLayout = d3.treemap(); //create the treemap
+
+    treemapLayout.size([w, h]) //configure the width and height of the treemap
+        .paddingOuter(2); //configure the outer padding
+    
+    treemapLayout(root); //call treemapLayout by passing in the root hierarchy object
+
+    /** Tiles */
+    svg.append("g") //join nodes to rect elements and update the x, y, width, and height properties of each rect
+        .selectAll("rect") 
+        .attr("class", "tile")
+        .data(root.descendants()) //returns a flat array of root's descendants
+        .enter()
+        .append("rect")
+        .attr("x", d => d.x0)
+        .attr("y", d => d.y0)
+        .attr("width", d => d.x1 - d.x0)
+        .attr("height", d => d.y1 - d.y0)
+        .style("stroke", "black")
+        .style("fill", "lightblue")
+
+    /** Text labels */
+    svg.append("g")
+        .selectAll("text")
+        .data(root.descendants())
+        .enter()
+        .append("text")
+        .attr("x", d => d.x0 + 5)
+        .attr("y", d => d.y0 +20)
+        .text( d => d.data.name)
+        .attr("font-size", "0.5em")
+        .attr("fill", "black")
+
+    
+    /** Text labels */
+/*
+    svg.selectAll("text")
+        .data(root.leaves())
+        .enter()
+        .append("text")
+        .attr("x", d => d.x0 + 5)
+        .attr("y", d => d.y0 +20)
+        .text( d=> d.name)
+        .attr("font-size", "1em")
+        .attr("fill", "white")
+*/
 
     /** Debug */
-    document.getElementById("debug").innerHTML = videoGameData.name
+    document.getElementById("debug").innerHTML = videoGameData.children
 
     
     } //closes out the last then statement
