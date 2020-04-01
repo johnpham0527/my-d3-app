5db682abc9ac7da51fee4e0a05c47a6db95ccc02
John Pham <mr.john.pham@gmail.com>
2020-04-01 14:34:33 -0400

    Add wrap helper function

--- treemap.js
+++ treemap.js
@@ -21,8 +21,30 @@ const w = 1100;
 const h = 700;
 const padding = 80;
 
-/* Get Functions */
-
+/* Helper Functions */
+function wrap(text, width) {
+    text.each(function() {
+      var text = d3.select(this),
+          words = text.text().split(/\s+/).reverse(),
+          word,
+          line = [],
+          lineNumber = 0,
+          lineHeight = 1.1, // ems
+          y = text.attr("y"),
+          dy = parseFloat(text.attr("dy")),
+          tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+      while (word = words.pop()) {
+        line.push(word);
+        tspan.text(line.join(" "));
+        if (tspan.node().getComputedTextLength() > width) {
+          line.pop();
+          tspan.text(line.join(" "));
+          line = [word];
+          tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+        }
+      }
+    });
+  }
 
 /* SVG const */
 const svg = d3.select("#treemap")
@@ -70,6 +92,7 @@ fetch(VIDEO_GAME_SALES_URL)
         .data(root.leaves()) //returns a flat array of nodes with no children
         .enter()
         .append("text")
+
         .attr("x", d => d.x0 + 5)
         .attr("y", d => d.y0 + 20)
         .text( d => d.data.name)
