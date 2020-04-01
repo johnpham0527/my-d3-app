e2cdf315e304f2e04012cccc4e93e17da6ec3ab7
John Pham <mr.john.pham@gmail.com>
2020-04-01 10:12:00 -0400

    Create treemap.html

--- heatmap.js
+++ heatmap.js
@@ -170,14 +170,11 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
             .call(yAxis);
 
         /** Legend axis */
-        /*
         svg.append("g")
             .attr("transform", "translate(" + (w-padding-legendCellWidth*colorArray.length) + "," + (padding/12 + cellHeight - 1) + ")")
             .call(legendAxis);
-        */
 
         /** Legend cells */
-        /*
         svg.append("g")
             .attr("id","legend")
             .selectAll("rect")
@@ -189,6 +186,5 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
             .attr("width", legendCellWidth)
             .attr("height", cellHeight)
             .style("fill", (d) => d)
-        */
 
 }); // Closing brace for last then statement and closing parenthesis for fetch statement
