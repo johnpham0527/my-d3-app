e2a387e861f5377e0fe646ac8f7161ddaf1d82f7
John Pham <mr.john.pham@gmail.com>
2020-04-02 12:51:11 -0400

    Attempt to implement new wrapText algorithm

--- treemap.js
+++ treemap.js
@@ -23,6 +23,21 @@ const padding = 80;
 
 /* Helper Functions */
 const wrapText = (text, width, height) => {
+/* Algorithm:
+1. split all text by space
+2. check width. if too large, cut off after wordWidth spaces
+3. check height. if too large, cut off after wordHeight spaces
+*/
+    let maxWordWidth = Math.floor(width/7); //I estimate that each characters requires 7 pixels of width
+    let maxTextLines = Math.floor(height/16); //I estimate that each text line requires 16 pixels of text
+
+    let textArray = text.split(" ") //splice the text by space into an array of text lines
+        .forEach( textLine => textLine.slice(0,maxWordWidth)); //for each line, trim all text that exceeds the max calculated width
+
+    textArray.splice(0, maxTextLines); //trim any lines past the maximum number of text lines possible
+    return textArray;
+
+/*
     let estimatedTextWidth = text.length * 7; //I estimate that each characters requires 7 pixels of width
     if (estimatedTextWidth < width) {
         return [text];
@@ -45,6 +60,7 @@ const wrapText = (text, width, height) => {
         textArray.push(word)
         return textArray;
     }
+*/
 }
 
 
