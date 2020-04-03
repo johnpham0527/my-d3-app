0ca640f279cd3d12c33bcc94ddbd231c97d3eadd
John Pham <mr.john.pham@gmail.com>
2020-04-03 08:06:18 -0400

    Use light blue instead of blue color

--- treemap.js
+++ treemap.js
@@ -70,7 +70,7 @@ fetch(VIDEO_GAME_SALES_URL)
     treemapLayout(root); //call treemapLayout by passing in the root hierarchy object
 
     /** Color */
-    let colorArray = ["red", "orange", "yellow", "green", "blue", "violet",
+    let colorArray = ["red", "orange", "yellow", "green", "lightblue", "violet",
     "#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c",
     "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928"];
     
